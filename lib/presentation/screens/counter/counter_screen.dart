fa3fe20e305404a30dc64f866babbb2d126f8a70
ScrobeZ <jorgeluis-sc@outlook.es>
2023-10-10 20:25:57 -0700

    first commit

---
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+  @override
+  State<Counter> createState() => _CounterState();
+}
+class _CounterState extends State<Counter> {
+  int countador = 10;
+  String text = "Clicks";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$countador',
+              style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+          Text(text, 
+          style: const TextStyle(fontSize: 60))
+          ],
+        ),
+      ),
+      floatingActionButton:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+                setState(() {
+                countador ++;
+                if (countador > 0) {
+                text = "Clicks";
+                }
+                });
+            },
+              child: const Icon(Icons.plus_one),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () {
+                setState(() {
+                if (countador > 0) {
+                  countador --;
+                  text = "No se puede bajar de 0";
+                  }
+                });
+              }, child: const Icon(Icons.exposure_minus_1)),
+        ],
+      ),
+    );
+  }
+}
